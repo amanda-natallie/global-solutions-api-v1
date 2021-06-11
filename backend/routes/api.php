ffbe1532581be783c26e34e54fdb652e324551e1
Amanda Natallie <amanda.natallie.2@gmail.com>
2021-06-11 18:07:45 -0300

    add find by id

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,18 +18,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::namespace('API')->name('api.')->group(function(){
-    Route::prefix('/carwash')->group(function(){
-        Route::get('/getAll',[\App\Http\Controllers\Api\CarwashController::class,"findAll"])->name("findAll");
-        Route::post('/create',[\App\Http\Controllers\Api\CarwashController::class,"create"])->name("create");
-        Route::post('/update',[\App\Http\Controllers\Api\CarwashController::class,"update"])->name("update");
-        Route::post('/upload',[\App\Http\Controllers\Api\CarwashController::class,"upload"])->name("upload");
-        Route::post('/remove',[\App\Http\Controllers\Api\CarwashController::class,"remove"])->name("remove");
-    });
     Route::prefix('/car')->group(function(){
         Route::get('/getAll',[\App\Http\Controllers\Api\CarController::class,"findAll"])->name("findAll");
+        Route::post('/getById',[\App\Http\Controllers\Api\CarController::class,"findByid"])->name("findByid");
         Route::post('/create',[\App\Http\Controllers\Api\CarController::class,"create"])->name("create");
         Route::post('/update',[\App\Http\Controllers\Api\CarController::class,"update"])->name("update");
         Route::post('/upload',[\App\Http\Controllers\Api\CarController::class,"upload"])->name("upload");
         Route::post('/remove',[\App\Http\Controllers\Api\CarController::class,"remove"])->name("remove");
+    });
+    Route::prefix('/carwash')->group(function(){
+        Route::get('/getAll',[\App\Http\Controllers\Api\CarwashController::class,"findAll"])->name("findAll");
+        Route::post('/getById',[\App\Http\Controllers\Api\CarwashController::class,"findByid"])->name("findByid");
+        Route::post('/create',[\App\Http\Controllers\Api\CarwashController::class,"create"])->name("create");
+        Route::post('/update',[\App\Http\Controllers\Api\CarwashController::class,"update"])->name("update");
+        Route::post('/remove',[\App\Http\Controllers\Api\CarwashController::class,"remove"])->name("remove");
     });
 });
